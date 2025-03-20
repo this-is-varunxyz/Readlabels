843755c25e6a8008c2bef208a0d676967733e33b
this-is-varunxyz <varunkushwah500notes@gmail.com>
2025-03-20 14:41:30 +0530

    added nodejs

---
+++ server.js
@@ -0,0 +1,36 @@
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+const express = require('express')
+const app = express();
+require("dotenv").config();
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run() {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage("tell_me_a_joke");
+  console.log(result.response.text());
+}
+
+
+run();
