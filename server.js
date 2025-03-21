8a5c3628e715baaec0df408252ececf9f8df62ac
this-is-varunxyz <varunkushwah500notes@gmail.com>
2025-03-21 17:02:35 +0530

    working on image

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const {
 } = require("@google/generative-ai");
 const express = require('express')
 const path = require('path');
+const multer = require('multer');
 const { json } = require("stream/consumers");
 const app = express();
 app.set("view engine",'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,"public")))
+const storage = multer.diskStorage({
+  destination:(req,file,cb)=>{
+    return cb(null,"./public/uploads");
 
+  },
+  filename: (req,file,cb)=>{
+return cb(null,'productimage')
+  }
+})
+const upload = multer({storage})
 require("dotenv").config();
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
@@ -21,19 +31,23 @@ app.get('/', function (req, res) {
 
 
 })
-app.post('/result', async (req, res) => {
-  try {
-    let answer = JSON.parse(await run(`give me a object in javascript and dont write a single thing extra just give this thing {"name":xyz,"cost":xyz} replace xyz with anythign else and dont even write javascirpt in bigning dont declare this too`));
+app.post('/result', upload.single('imageFile'),async (req, res) => {
+  console.log(req.body.imageFile);
+ 
+res.render("result",{image:`uploads/productimage`});
+  // try {
+  //   let answer = JSON.parse(await run(`give me a object in javascript and dont write a single thing extra just give this thing {"name":xyz,"cost":xyz} replace xyz with anythign else and dont even write javascirpt in bigning dont declare this too`));
     
-    // Combine all template variables in one object
-    res.render('result', {
-      answer: answer,
-      image: req.body.imageFile
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("An error occurred");
-  }
+  //   // Combine all template variables in one object
+  //   res.render('result', {
+  //     answer: answer,
+  //     image: req.body.imageFile
+      
+  //   });
+  // } catch (error) {
+  //   console.error("Error:", error);
+  //   res.status(500).send("An error occurred");
+  // }
 });
 
 const model = genAI.getGenerativeModel({
